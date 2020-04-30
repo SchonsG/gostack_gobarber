e630e54d13d1e7815c79a8956f4c787750899d99
Guilherme Schons <gschonswow@gmail.com>
2020-04-30 02:53:16 -0300

    Created appointments manager

---
+++ src/database/index.ts
@@ -0,0 +1,3 @@
+import { createConnection } from 'typeorm';
+
+createConnection(); // O próprio createConnetion procura o arquivo de configuração e seta automaticamente
---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,36 @@
+import { Router } from 'express';
+import { parseISO } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
+
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
+
+const appointmentsRouter = Router();
+
+appointmentsRouter.get('/', async (request, response) => {
+  const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+  const appointments = await appointmentsRepository.find();
+
+  return response.status(200).json(appointments);
+});
+
+appointmentsRouter.post('/', async (request, response) => {
+  try {
+    const { provider, date } = request.body;
+
+    const parsedDate = parseISO(date);
+
+    const CreateAppointment = new CreateAppointmentService();
+
+    const appointment = await CreateAppointment.execute({
+      provider,
+      date: parsedDate,
+    });
+
+    return response.status(200).json(appointment);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+export default appointmentsRouter;
